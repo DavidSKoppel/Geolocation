5b5e218df830dbf4571549cfaad77ee27b54862d
DavidSKoppel <71077820+DavidSKoppel@users.noreply.github.com>
2022-03-22 12:47:09 +0100

    Initial commit

---
+++ Geolocation/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Geolocation.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Geolocation.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Geolocation/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Geolocation.Models;
+using Geolocation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Geolocation.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
