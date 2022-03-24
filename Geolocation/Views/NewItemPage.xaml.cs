62aab00d6a174b36b13bf65fd993d9ca18485615
DavidSKoppel <71077820+DavidSKoppel@users.noreply.github.com>
2022-03-24 08:17:12 +0100

    Added Geolocation Functions
    Returns latitude and longitude

--- Geolocation/Views/NewItemPage.xaml.cs
+++ Geolocation/Views/NewItemPage.xaml.cs
@@ -3,8 +3,10 @@ using Geolocation.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace Geolocation.Views
 {
@@ -16,6 +18,30 @@ namespace Geolocation.Views
         {
             InitializeComponent();
             BindingContext = new NewItemViewModel();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Xamarin.Essentials.Geolocation.GetLastKnownLocationAsync();
+                if(location == null)
+                {
+                    location = await Xamarin.Essentials.Geolocation.GetLocationAsync(new GeolocationRequest
+                    { 
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(10)
+                    });
+                }
+                if (location != null)
+                    LabelLocation.Text = $"{location.Latitude} {location.Longitude}";
+                else
+                    LabelLocation.Text = "No GPS";
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Something is wrong: {ex.Message}");
+            }//Deez nuts
         }
     }
 }
