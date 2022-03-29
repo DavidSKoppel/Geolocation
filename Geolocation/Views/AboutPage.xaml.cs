5088b8f0b0e226fec121917cb1fe85fd16c78740
DavidSKoppel <71077820+DavidSKoppel@users.noreply.github.com>
2022-03-29 10:19:18 +0200

    Added map
    Added a map that shows the world and your current location

--- Geolocation/Views/AboutPage.xaml.cs
+++ Geolocation/Views/AboutPage.xaml.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace Geolocation.Views
@@ -10,6 +17,50 @@ namespace Geolocation.Views
         public AboutPage()
         {
             InitializeComponent();
+
+            Task.Delay(2000);
+            //UpdateMap();
         }
+
+
+        List<Place> placesList = new List<Place>();
+
+        /*private async void UpdateMap()
+        {
+            try
+            {
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(AboutPage)).Assembly;
+                Stream stream = assembly.GetManifestResourceStream("MapApp.Places.json");
+                string text = string.Empty;
+                using (var reader = new StreamReader(stream))
+                {
+                    text = reader.ReadToEnd();
+                }
+
+                var resultObject = JsonConvert.DeserializeObject<Places>(text);
+
+                foreach (var place in resultObject.results)
+                {
+                    placesList.Add(new Place
+                    {
+                        PlaceName = place.name,
+                        Address = place.vicinity,
+                        Location = place.geometry.location,
+                        Position = new Position(place.geometry.location.lat, place.geometry.location.lng),
+                        //Icon = place.icon,
+                        //Distance = $"{GetDistance(lat1, lon1, place.geometry.location.lat, place.geometry.location.lng, DistanceUnit.Kiliometers).ToString("N2")}km",
+                        //OpenNow = GetOpenHours(place?.opening_hours?.open_now)
+                    });
+                }
+                //MyMap.ItemsSource = placesList;
+                //PlacesListView.ItemsSource = placesList;
+                //var loc = await Xamarin.Essentials.Geolocation.GetLocationAsync();
+                //MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(47.6370891183, -122.123736172), Distance.FromKilometers(1)));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }*/
     }
 }
